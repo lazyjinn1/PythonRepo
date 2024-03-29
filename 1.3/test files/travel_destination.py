46fa9043af0dd79b22f224b8c14f70d089147320
lazyjinn1 <joshjbarba@gmail.com>
2024-03-28 19:08:46 -0700

    Updated for 1.3

---
+++ 1.3/test files/travel_destination.py
@@ -0,0 +1,15 @@
+travel_destinations = ['Japan', 'Korea', 'China'] 
+
+user_destination = str(input('Where would you like to go? '))
+
+if(user_destination in travel_destinations): 
+    print('Enjoy your stay in ' + user_destination) 
+elif(user_destination not in travel_destinations): 
+
+    print('Oops, that destination is not currently available.') 
+
+         
+
+ 
+
+ 
