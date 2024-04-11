b9867d3973ca5749966bbbb685b668793e631baa
lazyjinn1 <joshjbarba@gmail.com>
2024-04-10 17:36:11 -0700

    Adding screenshots

--- Achievement 2/2.3/recipe_app/recipes/models.py
+++ Achievement 2/2.3/recipe_app/recipes/models.py
@@ -5,7 +5,7 @@ class Recipe(models.Model):
     ingredients = models.CharField(max_length = 200)
     cooking_time = models.FloatField(help_text='in minutes')
     difficulty = models.CharField(max_length = 20)
-    note = models.TextField()
+    note = models.TextField(blank=True)
 
     def __str__(self):
         return str(self.recipe_name)
